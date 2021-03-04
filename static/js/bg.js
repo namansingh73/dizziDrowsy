d1431d8086ae74a29fce17537d3d54652c764c11
Naman Singh <snaman431@gmail.com>
2021-03-04 22:16:58 +0530

    added background file

---
+++ static/js/bg.js
@@ -0,0 +1,12 @@
+includeHTML = async () => {
+    const element = document.body.querySelector(".bg-animated");
+    // console.log(element);
+    const file = element.getAttribute("include-html");
+    // console.log(file);
+    const data = await fetch(file).then((respose) => respose.text());
+    console.log(data);
+    element.innerHTML = data;
+  };
+  
+  includeHTML();
+  
